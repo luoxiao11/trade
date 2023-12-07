ffb55a6708dd53fbdb98f219ec8101a8baeac6cf
luoxiao11 <luoxiao.rf@gmail.com>
2023-12-07 13:04:13 -1000

    commit18: branch 13- shading

--- trade-order/src/test/java/com/shangan/trade/order/OrderTest.java
+++ trade-order/src/test/java/com/shangan/trade/order/OrderTest.java
@@ -2,6 +2,7 @@ package com.shangan.trade.order;
 
 import com.shangan.trade.order.db.dao.OrderDao;
 import com.shangan.trade.order.db.model.Order;
+import com.shangan.trade.order.utils.SnowflakeIdWorker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +16,23 @@ import java.util.Date;
 public class OrderTest {
     @Autowired
     private OrderDao orderDao;
+    private SnowflakeIdWorker snowFlake = new SnowflakeIdWorker(6, 8);
 
     @Test
     public void insertOrderTest(){
-        Order order = new Order();
-        order.setUserId(123456L);
-        order.setGoodsId(123L);
-        order.setPayTime(new Date());
-        order.setPayPrice(16500);
-        order.setStatus(0);
-        boolean insertresult = orderDao.insertOrder(order);
-        System.out.println(insertresult);
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello");
+            Order order = new Order();
+            order.setId(snowFlake.nextId() + 1);
+            order.setUserId(123456788L + i);
+            order.setGoodsId(12378L);
+            order.setPayTime(new Date());
+            order.setPayPrice(1999);
+            order.setStatus(1);
+            order.setActivityType(1);
+            order.setCreateTime(new Date());
+            boolean insertresult = orderDao.insertOrder(order);
+            System.out.println(insertresult);
+        }
     }
 }
